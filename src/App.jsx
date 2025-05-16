f9dcf341af5134e5a73805b6b934b2df0f1df765
Your Name <you@example.com>
2025-05-16 21:46:53 +0300

    Fix footer path case sensitivity issue

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Navbar from './components/navbar/Navbar'
-import Footer from './components/Footer/Footer'
+import Footer from './components/footer/Footer'
 import Home from './pages/home/Home'
 import Create from './pages/create/Create'
 import Details from './pages/details/Details'
